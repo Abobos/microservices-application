9c26e84e2c2910309f8e3ffff30390bb93e11d1b
abobos <blessingmakaraba@gmail.com>
2022-09-23 22:47:29 +0100

    Updates deployment yaml

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "afb508471afc647deb47f08f5aa2734d-1025852732.us-west-1.elb.amazonaws.com/api/v0",
 };
 
 /*
